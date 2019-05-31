c34ce314a3582f0045aeb7c565d65a37208f8fbe
abhinavjnvb <abhinavjnvb@gmail.com>
2019-06-01 01:03:05 +0530

    create:basic layout and card view of profile

---
+++ portfolio/src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './App.css';
+import pic from './assets/Abhinav.jpg'
+import { Layout, Menu, Card, Icon } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+const { Meta } = Card;
+
+export default class App extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+  render(){
+    return (
+      <div className="App">
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+            <div className="logo" />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1">
+                <Icon type="pie-chart" />
+                <span>Option 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="desktop" />
+                <span>Option 2</span>
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="user" />
+                    <span>User</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="3">Tom</Menu.Item>
+                <Menu.Item key="4">Bill</Menu.Item>
+                <Menu.Item key="5">Alex</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="team" />
+                    <span>Team</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="6">Team 1</Menu.Item>
+                <Menu.Item key="8">Team 2</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="9">
+                <Icon type="file" />
+                <span>File</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }} />
+            <Content style={{ margin: '24px' }}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src={pic} />}
+              >
+                <Meta title="Abhinav" description="www.frulix.com" />
+              </Card>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>©2019 Created by Abhinav</Footer>
+          </Layout>
+        </Layout>
+      </div>
+    );
+  }
+  
+}
+
+
